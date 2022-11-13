fc01b3711e34c6a3ff765a7ccb7d769d0c5798c0
lexyblazy <adekola.olalekan01@gmail.com>
2022-11-13 18:04:52 +0100

    improve locker.ts

--- packages/app/src/redisHelper/locker.ts
+++ packages/app/src/redisHelper/locker.ts
@@ -1,33 +1,21 @@
 import Redlock, { Lock, ResourceLockedError } from "redlock";
 import { getClient } from "./init";
 
-const redisClient = getClient();
-
 const DEFAULT_LOCK_DURATION = 1000;
 
-const redlock = new Redlock([redisClient], {
-  driftFactor: 0.01,
-
-  retryCount: 10,
-
-  retryDelay: 200,
-
-  retryJitter: 200,
-  automaticExtensionThreshold: 500,
-});
+let redlockInstance: Redlock;
 
 export const withLock = async <T>(
   lockResource: string,
-  callback: (lock: Lock) => Promise<T>,
+  callback: (lock: Lock | null) => Promise<T>,
   duration = DEFAULT_LOCK_DURATION
 ) => {
   let lock: Lock | null = null;
   try {
-    lock = await redlock.acquire([lockResource], duration);
-    return callback(lock);
-  } catch (error) {
-    console.log(`Failed to acquire lock on ${lockResource}`);
-    return null;
+    const fullLockResource = `Redlock:${lockResource}`;
+
+    lock = await redlockInstance.acquire([fullLockResource], duration);
+    return await callback(lock);
   } finally {
     if (lock) {
       await lock.release();
@@ -35,10 +23,25 @@ export const withLock = async <T>(
   }
 };
 
-redlock.on("error", (error) => {
-  if (error instanceof ResourceLockedError) {
-    return;
-  }
+export const init = () => {
+  const redisClient = getClient();
 
-  console.error(error);
-});
+  redlockInstance = new Redlock([redisClient], {
+    driftFactor: 0.01,
+
+    retryCount: 10,
+
+    retryDelay: 200,
+
+    retryJitter: 200,
+    automaticExtensionThreshold: 500,
+  });
+
+  redlockInstance.on("error", (error) => {
+    if (error instanceof ResourceLockedError) {
+      return;
+    }
+
+    console.error(error);
+  });
+};
