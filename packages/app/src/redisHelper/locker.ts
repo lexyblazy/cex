8ed33df6a9334ed9aa0e5a808b8fc4fa45be8227
lexyblazy <adekola.olalekan01@gmail.com>
2022-11-16 11:41:50 +0100

    add lock extension helper

--- packages/app/src/redisHelper/locker.ts
+++ packages/app/src/redisHelper/locker.ts
@@ -1,7 +1,18 @@
-import Redlock, { Lock, ResourceLockedError } from "redlock";
+import Redlock, { Lock, ResourceLockedError, Settings as RedLockSettings } from "redlock";
 import { getClient } from "./init";
 
-const DEFAULT_LOCK_DURATION = 1000;
+const DEFAULT_LOCK_DURATION = 3000;
+
+const redlockSettings: RedLockSettings = {
+  driftFactor: 0.01,
+
+  retryCount: 10,
+
+  retryDelay: 200,
+
+  retryJitter: 200,
+  automaticExtensionThreshold: 500,
+};
 
 let redlockInstance: Redlock;
 
@@ -12,7 +23,7 @@ export const withLock = async <T>(
 ) => {
   let lock: Lock | null = null;
   try {
-    const fullLockResource = `Redlock:${lockResource}`;
+    const fullLockResource = prefixLockResource(lockResource);
 
     lock = await redlockInstance.acquire([fullLockResource], duration);
     return await callback(lock);
@@ -23,25 +34,46 @@ export const withLock = async <T>(
   }
 };
 
+const prefixLockResource = (lockResource: string) => `Redlock:${lockResource}`;
+
+const getLockResource = (lock: Lock) => lock.resources[0];
+
+export const extendLock = async (lock: Lock, lockResource: string, duration: number) => {
+  const isLockValid = isLockStillValid(lock);
+
+  const isCorrectResource = getLockResource(lock) === prefixLockResource(lockResource);
+
+  if (!isCorrectResource) {
+    throw new Error(
+      `Lock resource mismatch!!!, ${getLockResource(lock)} is similar to ${prefixLockResource(
+        lockResource
+      )} `
+    );
+  }
+
+  if (isLockValid) {
+    await lock.extend(duration);
+  } else {
+    throw new Error(`Lock on ${lockResource} has already expired`);
+  }
+};
+
+export const isLockStillValid = (lock: Lock) => {
+  const remainingTimeMS = lock.expiration - Date.now();
+
+  return remainingTimeMS > 0;
+};
+
 export const init = () => {
   const redisClient = getClient();
 
-  redlockInstance = new Redlock([redisClient], {
-    driftFactor: 0.01,
-
-    retryCount: 10,
-
-    retryDelay: 200,
-
-    retryJitter: 200,
-    automaticExtensionThreshold: 500,
-  });
+  redlockInstance = new Redlock([redisClient], redlockSettings);
 
   redlockInstance.on("error", (error) => {
     if (error instanceof ResourceLockedError) {
       return;
     }
 
-    console.error(error);
+    console.error("Redlock Error", error);
   });
 };
