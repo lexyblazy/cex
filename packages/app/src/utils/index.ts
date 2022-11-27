9773550753cbd41f21b996fda136d6b9b477f3c3
lexyblazy <adekola.olalekan01@gmail.com>
2022-11-27 12:21:48 +0100

    add isValidUUid helper

--- packages/app/src/utils/index.ts
+++ packages/app/src/utils/index.ts
@@ -17,3 +17,10 @@ export const getCurrentCommitHash = () => {
     });
   });
 };
+
+export const isValidUUid = (input: string) => {
+  const regexExp =
+    /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+
+  return regexExp.test(input);
+};
