cffa4cbbf8d1c06d1e8f96ae99f16cdb5eafad13
lexyblazy <adekola.olalekan01@gmail.com>
2022-11-29 02:58:55 +0100

    add rateLimiting to getDepositAddress endpoint

--- packages/app/src/addresses/handlers/getDepositAddress.ts
+++ packages/app/src/addresses/handlers/getDepositAddress.ts
@@ -4,8 +4,9 @@ import _ from "lodash";
 import * as typeorm from "typeorm";
 
 import { AddressEntity, addressEntity, assetEntity } from "#/db/schemas";
-import { isValidUUid } from "#/utils";
 import { getOneWithLock } from "#/db/utils";
+import { isValidUUid } from "#/utils";
+import { withRedlock } from "#/redisHelper/locker";
 
 export const getDepositAddress: express.RequestHandler = async (req, res) => {
   const { assetId } = req.params;
@@ -51,25 +52,52 @@ export const getDepositAddress: express.RequestHandler = async (req, res) => {
     return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send();
   }
 
-  const result = await getOneWithLock<AddressEntity>({
-    repository: addressRepository,
-    where: {
-      id: freeAddress.id,
-    },
-    relations: ["asset"],
-    callback: async (addressEntityLocked, transactionalEntityManager) => {
-      if (!addressEntityLocked) {
-        return null;
-      }
+  /**
+   *  Initially Implemented this to use only a database row level lock `getOneWithLock` ,
+   * so other attempts to lock the row will be queued up until the current operation releases the lock.
+   *
+   *  However this will stress the database if there are multiple requests
+   * (with each one attempting to obtain a exclusive lock  on the resource)
+   *
+   * So I decided to leverage redlock to implement a lock at the application level,
+   * then return a `rateLimited` response
+   *
+   */
 
-      const transactionalAddressRepository =
-        transactionalEntityManager.getRepository(addressEntity);
+  const lockResource = `getDepositAddress:${freeAddress.address}`;
 
-      addressEntityLocked.user = user;
+  const result = await withRedlock(lockResource, async (lock) => {
+    if (!lock) {
+      return null;
+    }
 
-      return transactionalAddressRepository.save(addressEntityLocked);
-    },
+    const dbLockResult = await getOneWithLock<AddressEntity>({
+      repository: addressRepository,
+      where: {
+        id: freeAddress.id,
+      },
+      relations: ["asset"],
+      callback: async (addressEntityLocked, transactionalEntityManager) => {
+        if (!addressEntityLocked) {
+          console.log(`Failed to get address Entity locked, id =${freeAddress.id}`);
+          return null;
+        }
+
+        const transactionalAddressRepository =
+          transactionalEntityManager.getRepository(addressEntity);
+
+        addressEntityLocked.user = user;
+
+        return transactionalAddressRepository.save(addressEntityLocked);
+      },
+    });
+
+    return dbLockResult;
   });
+
+  if (!result) {
+    return res.status(HttpStatus.TOO_MANY_REQUESTS).send({ error: "rateLimited" });
+  }
 
   return res.send(_.omit(result, "user"));
 };
