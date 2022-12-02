0286151295dbf5e4648b0f72568f97d7f48bfec9
lexyblazy <adekola.olalekan01@gmail.com>
2022-12-02 21:25:31 +0100

    add boiler plate code for transction route

--- packages/app/src/transactions/handlers/createTransaction.ts
+++ packages/app/src/transactions/handlers/createTransaction.ts
@@ -1,5 +1,15 @@
 import * as express from "express";
+import { getWorker, constants } from "#/workers";
 
-export const createTransaction: express.RequestHandler = (req, res) => {
+import { TransactionsJobPipeline } from "../types";
+
+export const createTransaction: express.RequestHandler = async (req, res) => {
+  const transactionWorker = getWorker<TransactionsJobPipeline>(constants.TRANSACTIONS_JOB_PIPELINE);
+
+  await transactionWorker.add({
+    action: "createTransaction",
+    payload: req.body,
+  });
+
   res.send("Creating transaction");
 };
